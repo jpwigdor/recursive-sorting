7d4d5d6316534164659a2d975b54e7807f48d7ef
James Wigdor <jpwigdor@gmail.com>
2020-06-16 19:13:42 -0400

    complete sorting algorithm

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -3,7 +3,28 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
-    # Your code here
+    arrA_index = 0
+    arrB_index = 0
+    index = 0
+    while arrA_index < len(arrA) and arrB_index < len(arrB):
+        if arrA[arrA_index] < arrB[arrB_index]:
+            merged_arr[index] = arrA[arrA_index]
+            index += 1
+            arrA_index += 1
+        else:
+            merged_arr[index] = arrB[arrB_index]
+            index += 1
+            arrB_index += 1
+
+    while arrA_index < len(arrA):
+        merged_arr[index] = arrA[arrA_index]
+        index += 1
+        arrA_index += 1
+
+    while arrB_index < len(arrB):
+        merged_arr[index] = arrB[arrB_index]
+        index += 1
+        arrB_index += 1
 
     return merged_arr
 
@@ -12,7 +33,14 @@ def merge(arrA, arrB):
 
 def merge_sort(arr):
     # Your code here
-
+    start = 0
+    end = len(arr)
+    if end > start + 1:
+        mid = int((start + end) / 2)
+        # print(arr[0:mid], arr[mid:end], start, mid, end)
+        arrA = merge_sort(arr[0:mid])
+        arrB = merge_sort(arr[mid:end])
+        arr = merge(arrA, arrB)
     return arr
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't
@@ -23,7 +51,9 @@ def merge_sort(arr):
 
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    pass
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+    pass
