6e966063871e763f5c312f815a73e27a6fd2cfa9
James Wigdor <jpwigdor@gmail.com>
2020-06-16 19:10:17 -0400

    complete searching algorithm

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,17 @@
 # TO-DO: Implement a recursive implementation of binary search
-def binary_search(arr, target, start, end):
-    # Your code here
+def binary_search(arr, target, low, high):
+    # "loop" if high is greater than or equal to low (checking base case)
+    if high >= low:
+        mid = (high + low) // 2  # then declare a mid point for now
+        if arr[mid] > target:  # If element is greater than mid, then it's in the left subarray
+            # recurse, towards base case
+            return binary_search(arr, target, low, mid - 1)
+        elif arr[mid] < target:  # If element is less than mid, then it's in the right subarray
+            # recurse, towards base case
+            return binary_search(arr, target, mid + 1, high)
+        elif arr[mid] == target:  # If element is the target
+            return mid  # then do not recurse, simply return that element
+    return -1  # not found (target was not in the list)
 
     # STRETCH: implement an order-agnostic binary search
     # This version of binary search should correctly find
@@ -12,3 +23,4 @@ def binary_search(arr, target, start, end):
 
 def agnostic_binary_search(arr, target):
     # Your code here
+    pass
